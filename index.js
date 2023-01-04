e8b1941a3b03a7d953646180cdb8caaa8d50b009
Christian Riaño <christianrv.dev@gmail.com>
2023-01-04 15:55:00 -0500

    Implement searchCity into the menu

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const { showMenu, doPause, askCity } = require("./src/helpers/inquirer");
+const Searches = require("./src/models/searches");
 
 const main = async () => {
   let hasLeft = false;
+
+  const searches = new Searches();
+
   do {
     const option = await showMenu();
     switch (option) {
       case 1:
         const city = await askCity();
+
+        searches.searchCity(city);
 
         break;
       case 2:
